b4b6b192b4a73e04428b8a35174c50b5621b8db3
phillip hall <philip.hall@justice.gsi.gov.uk>
2024-01-25 15:07:45 +0000

    Added functions for different types of table.

--- R/opg_tables.R
+++ R/opg_tables.R
@@ -1,16 +1,16 @@
-build_opg_tables <- function() {
+build_opg_tables <- function(SQL) {
 
 cover_df <- read.csv(system.file("pages/cover.csv", package = "opgstats"),check.names=FALSE)
 notes_df <- read.csv(system.file("pages/notes.csv", package = "opgstats"),check.names=FALSE)
 tables_df <- read.csv(system.file("pages/contents.csv", package = "opgstats"),check.names=FALSE)
 
-SQL <- "SELECT * FROM sirius_derived_dev_dbt.opg_family_court_stats_published"
+# SQL <- "SELECT * FROM sirius_derived_dev_dbt.opg_family_court_stats_published"
 
-data <- query_athena(SQL)
+data <- mojstats::query_athena(SQL)
 
 extract_date <- trimws(format(unique(data$extract_date), "%e %B %Y"))
 
-build_stats_tables(data,
+mojstats::build_stats_tables(data,
                    c("case_type","case_subtype","gender","age_band"),
                    "count",
                    title = "Office of the Public Guardian Statistics",
@@ -18,5 +18,17 @@ build_stats_tables(data,
                    notes_df,
                    tables_df,
                    "output.xlsx")
+
+}
+
+build_published_opg_tables <- function() {
+
+  build_opg_tables("SELECT * FROM sirius_derived_dev_dbt.opg_family_court_stats_published")
+
+}
+
+build_latest_opg_tables <- function() {
+
+  build_opg_tables("SELECT * FROM sirius_derived_dev_dbt.opg_family_court_stats_latest")
 
 }
